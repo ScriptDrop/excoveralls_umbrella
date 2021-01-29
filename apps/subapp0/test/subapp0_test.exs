e5493fb4bcb631f6ddeec6159bdb223bfd3ba95d
Matt Jones <jones@scriptdrop.co>
2021-01-29 18:07:26 -0500

    relocate test to subapp1

--- apps/subapp0/test/subapp0_test.exs
+++ apps/subapp0/test/subapp0_test.exs
@@ -2,9 +2,10 @@ defmodule SubApp0Test do
   use ExUnit.Case
   doctest SubApp0
 
-  test "plus" do
-    assert SubApp0.plus(1, 1) == 2
-  end
+  # NOTE: This test is now located in SubApp1's suite
+  # test "plus" do
+  #   assert SubApp0.plus(1, 1) == 2
+  # end
 
   test "minus" do
     assert SubApp0.minus(2, 1) == 1
--- apps/subapp1/test/subapp1_test.exs
+++ apps/subapp1/test/subapp1_test.exs
@@ -2,6 +2,10 @@ defmodule SubApp1Test do
   use ExUnit.Case
   doctest SubApp1
 
+  test "plus0" do
+    assert SubApp0.plus(1, 1) == 2
+  end
+
   test "plus" do
     assert SubApp1.plus(1, 1) == 2
   end
